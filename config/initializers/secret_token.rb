3d1646c0354ce6ed8e1dffaad348c9bf8c2d9b15
Martin Trejo <chilli.coder@gmail.com>
2013-12-15 13:02:01 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DryCrud::Application.config.secret_key_base = 'cb293e469780b85ea695b83c84b16ea261b69bfd401a4e0496f000ecf31eb730c043ade7b05a5d9345bd0b5b66e249c12bbd4d88dcd7197ea2e24b67c6a97528'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+DryCrud::Application.config.session_store :cookie_store, key: '_dry-crud_session'
